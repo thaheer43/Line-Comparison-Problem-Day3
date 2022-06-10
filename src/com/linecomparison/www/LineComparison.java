a547020c536fffccca30d7966dd21b33806a0637
“thaheer” <thaheer43@gmail.com>
2022-06-11 00:22:26 +0530

    Added: Logic for calculate length

--- src/com/linecomparison/www/LineComparison.java
+++ src/com/linecomparison/www/LineComparison.java
@@ -4,7 +4,13 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		
+		int x1=50, y1=100;
+		int x2=200,y2=400;
+		int x3=50, y3=100;
+		int x4=200,y4=400;
 		System.out.println("Welcome to Line Comparison Computation Program");
+		double length1=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		System.out.println("Length of line1: "+length1);
 		
 	}
 }
